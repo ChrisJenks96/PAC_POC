a8ab7a6ca099143fdcfe4a07c689b3faeee715bc
Chris Jenks <38247910+ChrisJenks96@users.noreply.github.com>
2020-02-27 23:14:05 +0000

    [JS] v0.04
    - Added keyboard input, synced position with speed vector, added keyup and keydown event listeners for user input.

--- js/MAIN.js
+++ js/MAIN.js
@@ -1,12 +1,9 @@
-//the function we invoke in HTML
-
-var main_sprite;
-
 //black, blue, green, cyan, red, magenta, brown, grey, 
 //dark grey, light blue, light green, light cyan, light red,
 //light magenta, yellow, white
 //this is an array of RGB values for CGA palette above
-var vga_palette = [
+var vga_palette =
+[
     0x00, 0x00, 0x00, //black 0
     0x00, 0x00, 0xA8, //blue 1
     0x00, 0xA8, 0x00, //green 2
@@ -25,10 +22,15 @@ var vga_palette = [
     0xFC, 0xFC, 0xFC //white 15
 ];
 
+var main_sprite;
+var platform;
+
+//the function we invoke in HTML
 function _start()
 {
     game_area.start();
-    main_sprite = new component(16, 16, PLAYER_buffer, PLAYER_buffer_size, 10, 10);
+    main_sprite = new component(16, 16, PLAYER_buffer, PLAYER_buffer_size, 20, 20);
+    platform = new component(16, 16, PLATFORM_buffer, PLATFORM_buffer_size, 20, 37);
 }
 
 //start by making the game screen area on the HTML page
@@ -44,6 +46,12 @@ var game_area =
             document.body.childNodes[0]);
         //call game area update every 20ms (50 fps)
         this.interval = setInterval(game_area_update, 20);
+        window.addEventListener('keydown', function (e) {
+            game_area.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            game_area.key = false;
+        })
     },
 
     //clear the canvas (like glClear)
@@ -60,10 +68,19 @@ function component(width, height, pix, pix_size, x, y)
     this.height = height;
     this.x = x;
     this.y = y;
+    this.speedx = 0;
+    this.speedy = 0;
    
+    this.new_pos = function ()
+    {
+        this.x += this.speedx;
+        this.y += this.speedy;
+    }
+
     //this is to be called every frame to update component 
     this.update = function (){
         ctx = game_area.context;
+        ctx.imageSmoothingEnabled = false;
         var image_data = ctx.createImageData(this.width, this.height);
         //16 colours, 2 cols per byte (4 bits each)
         //bit shift 4 (* 16) which will get 0-255
@@ -87,10 +104,24 @@ function component(width, height, pix, pix_size, x, y)
     }
 }
 
+function player_move_zero()
+{
+    main_sprite.speedx = 0;
+    main_sprite.speedy = 0;
+}
+
 function game_area_update()
 {
+    //move the player sprite
+    player_move_zero();
+    if (game_area.key && game_area.key == 37) { main_sprite.speedx = -1; }
+    if (game_area.key && game_area.key == 39) { main_sprite.speedx = 1; }
+
     //clear canvas
     game_area.clear();
     //render sprite
+    main_sprite.new_pos();
     main_sprite.update();
+    //update render platform
+    platform.update();
 }
